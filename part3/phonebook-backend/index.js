2f336e4ed99df0c68359ec134a56c824944f1569
hatemets <mistermark009@gmail.com>
2022-08-27 21:01:15 +0300

    3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,6 +4,29 @@ const cors = require("cors")
 const app = express()
 const port = 3001
 
+const people = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+]
+
 // Use cross-origin resource sharing (otherwise it wouldn't work on Mozilla Firefox)
 app.use(cors())
 
@@ -12,30 +35,14 @@ app.get("/", (req, res) => {
 })
 
 app.get("/api/persons", (req, res) => {
-    const people = [
-        { 
-            "id": 1,
-            "name": "Arto Hellas", 
-            "number": "040-123456"
-        },
-        { 
-            "id": 2,
-            "name": "Ada Lovelace", 
-            "number": "39-44-5323523"
-        },
-        { 
-            "id": 3,
-            "name": "Dan Abramov", 
-            "number": "12-43-234345"
-        },
-        { 
-            "id": 4,
-            "name": "Mary Poppendieck", 
-            "number": "39-23-6423122"
-        }
-    ]
-
     res.json(people)
+})
+
+app.get("/info", (req, res) => {
+    let content = `<p>Phonebook has info about ${people.length} people<p>`
+    content += `<p>${Date()}</p>`
+
+    res.send(content)
 })
 
 app.listen(port, () => {
