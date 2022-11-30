db71feb008d628e85d790af50858937adbb47ef0
mark <heidmetsmark@gmail.com>
2022-11-30 19:22:29 +0200

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,7 +2,11 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.map(blog => blog.likes).reduce((currLikes, total) => currLikes + total, 0)
 
+const favoriteBlog = (blogs) => blogs.reduce((curr, max) => curr.likes > max.likes ? curr : max)
+
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
