77a572725ba6ca069727f2469f377ab8944e290d
mark <heidmetsmark@gmail.com>
2022-12-05 21:29:40 +0200

    4.18

--- part4/app.js
+++ part4/app.js
@@ -3,10 +3,9 @@ const cors = require("cors")
 const config = require("./utils/config")
 const mongoose = require("mongoose")
 const logger = require("./utils/logger")
-
+const loginRouter = require("./controllers/login")
 const usersRouter = require("./controllers/users")
 const blogsRouter = require("./controllers/blogs")
-
 require("express-async-errors")
 
 const app = express()
@@ -15,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(usersRouter)
 app.use(blogsRouter)
+app.use("/api/login", loginRouter)
 
 logger.info("Connecting to", config.mongoUrl)
 
